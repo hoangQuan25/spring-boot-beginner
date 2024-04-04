9a0f737e668c37d028a3c232359b79defd3f15c4
hoangQuan25 <quan.nh215127@sis.hust.edu.vn>
2024-04-04 23:02:41 +0700

    spring mvc

--- 06-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/hoangquan/springboot/thymeleafdemo/controller/DemoController.java
+++ 06-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/hoangquan/springboot/thymeleafdemo/controller/DemoController.java
@@ -1,8 +1,16 @@
 package com.hoangquan.springboot.thymeleafdemo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DemoController {
-    
+
+    @GetMapping("/hello")
+    public String sayHello(Model theModel) {
+        theModel.addAttribute("theDate", java.time.LocalDateTime.now());
+
+        return "helloworld";
+    }
 }
